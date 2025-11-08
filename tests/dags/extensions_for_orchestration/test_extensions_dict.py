47cc0c81d3ae12395765ef7bdc5404c173d77753
k0rsakov <korsakov.iyu@gmail.com>
2025-11-08 14:44:56 +0300

    Изменён проект pet_project_testing_for_data_engineer: - Добавлены тесты для словарей

---
+++ tests/dags/extensions_for_orchestration/test_extensions_dict.py
@@ -0,0 +1,36 @@
+from dags.extensions_for_orchestration.extensions_dict import (
+    dict_keys_in_str,
+    dict_keys_in_placeholder,
+)
+
+class TestDictKeysInStr:
+    def test_empty_dict(self):
+        assert dict_keys_in_str({}) == ""
+
+    def test_none(self):
+        assert dict_keys_in_str(None) == ""
+
+    def test_single_key(self):
+        d = {"a": 1}
+        assert dict_keys_in_str(d) == "a"
+
+    def test_multiple_keys(self):
+        d = {"name": "Vasya", "age": 23, "country": "RU"}
+        assert dict_keys_in_str(d) == "name, age, country"
+
+
+class TestDictKeysInPlaceholder:
+    def test_empty_dict(self):
+        assert dict_keys_in_placeholder({}) == ""
+
+    def test_none(self):
+        assert dict_keys_in_placeholder(None) == ""
+
+    def test_single_key(self):
+        d = {"id": 42}
+        assert dict_keys_in_placeholder(d) == "%(id)s"
+
+    def test_multiple_keys(self):
+        d = {"name": "Ivan", "age": 30, "city": "Tomsk"}
+        assert dict_keys_in_placeholder(d) == "%(name)s, %(age)s, %(city)s"
+
