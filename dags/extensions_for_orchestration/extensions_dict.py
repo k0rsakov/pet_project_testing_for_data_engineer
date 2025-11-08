c85ab973c77d7fc6c4e13e7597f6505cb9bff93f
k0rsakov <korsakov.iyu@gmail.com>
2025-11-08 13:55:07 +0300

    Изменён проект pet_project_testing_for_data_engineer: - Добавлены ручки для словарей

---
+++ dags/extensions_for_orchestration/extensions_dict.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+
+def dict_keys_in_str(source_dict: dict[str, Any] | None = None) -> str:
+    """
+    Функция, которая генерирует строку с указанными колонками.
+
+    :param source_dict: Словарь, ключи которого необходимо преобразовать в строку; default 'None'
+    :return: str с указанными колонками через запятую
+    """
+    if source_dict is None:
+        return ""
+    return ", ".join(source_dict.keys())
+
+
+def dict_keys_in_placeholder(source_dict: dict[str, Any] | None = None) -> str:
+    """
+
+    :param source_dict:
+    :return:
+    """
+    if source_dict is None:
+        return ""
+    return ", ".join([f"%({k})s" for k in source_dict.keys()])
+
+
+# s = dict_keys_in_str(
+#     {
+#         'first_name': "Ivan",
+#         "last_name": "Ivanov",
+#         "email": 'foo@example.com'
+#     }
+# )
+#
+# print(
+#     f"""
+#     INSERT INTO ({s})
+#     VALUES()
+#     """
+# )
