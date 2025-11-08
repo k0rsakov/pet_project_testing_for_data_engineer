414036d17320eb365e5f342040fc68fc77bf0a45
k0rsakov <korsakov.iyu@gmail.com>
2025-11-08 09:49:37 +0300

    Изменён проект pet_project_testing_for_data_engineer: - добавлен пример тестирования функции

---
+++ tests/test_simple_function.py
@@ -0,0 +1,7 @@
+from main.simple_function import return_value
+
+def test_return_value_int():
+    assert return_value(5) == 5
+
+def test_return_value_str():
+    assert return_value("hello") == "hello"
