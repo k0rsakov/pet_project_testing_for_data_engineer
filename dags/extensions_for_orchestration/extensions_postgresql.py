9787be853b655cdc1b357ca55f88498e03b99077
k0rsakov <korsakov.iyu@gmail.com>
2025-11-08 13:56:30 +0300

    Изменён проект pet_project_testing_for_data_engineer: - Улучшена ручка для postgresql

--- dags/extensions_for_orchestration/extensions_postgresql.py
+++ dags/extensions_for_orchestration/extensions_postgresql.py
@@ -1,22 +1,64 @@
-from typing import Dict
+from typing import Any
+from airflow.models.connection import Connection
+import airflow.models.connection
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from extensions_for_orchestration.extensions_dict import dict_keys_in_str, dict_keys_in_placeholder
 
 
-def save_user_to_pg(user: Dict[str, str], conn_id: str, schema: str, table: str) -> None:
+def save_dict_to_postgres(
+    conn_id: str | None = "dwh",
+    schema: str | None = "public",
+    table: str | None = None,
+    dict_row: dict[str, Any] | None = None,
+) -> None:
     """
-    Сохраняет пользователя в PostgreSQL через PostgresHook.
-
-    :param user: Словарь с user-полями
-    :param conn_id: Имя соединения в Airflow (например, 'dwh')
-    :param schema: Схема БД в Postgres
+    Универсальная функция для вставки одной строки в любую таблицу (Postgres).
+    :param conn_id: Название соединения Airflow
+    :param schema: Схема Postgres
     :param table: Имя таблицы
+    :param dict_row: Произвольный dict со значениями для вставки (названия ключей = поля таблицы)
     """
     pg_hook = PostgresHook(postgres_conn_id=conn_id)
-    insert_sql = f"""
-        INSERT INTO {schema}.{table} (first_name, last_name, email, city, country)
-        VALUES (%(first_name)s, %(last_name)s, %(email)s, %(city)s, %(country)s);
-    """
-    pg_hook.run(
-        sql=insert_sql,
-        parameters=user,
-    )
+
+    if not dict_row:
+        raise ValueError("Нет данных для вставки (пустой словарь)")
+
+    if isinstance(conn_id, airflow.models.connection.Connection):
+        pg_hook = PostgresHook(postgres_conn_id=None, connection=conn_id)
+
+    columns = dict_keys_in_str(source_dict=dict_row)
+    placeholders = dict_keys_in_placeholder(source_dict=dict_row)
+    insert_sql = f"INSERT INTO {schema}.{table} ({columns}) VALUES ({placeholders})"
+    print(insert_sql)
+
+    pg_hook.run(sql=insert_sql, parameters=dict_row, autocommit=True)
+
+
+# conn_params = {
+#     "host": "localhost",
+#     "schema": "postgres",
+#     "login": "postgres",
+#     "password": "postgres",
+#     "port": 5433,
+# }
+
+# Создание объекта подключения вручную
+# conn = Connection(
+#     conn_type="postgres",
+#     host=conn_params["host"],
+#     schema=conn_params["schema"],
+#     login=conn_params["login"],
+#     password=conn_params["password"],
+#     port=conn_params["port"],
+# )
+
+# print(type(conn))
+#
+# if isinstance(conn, airflow.models.connection.Connection):
+#     print("yes")
+# else:
+#     print('not yes')
+
+# save_dict_to_postgres(
+#     conn_id=conn, table="users", row={"first_name": "Ivan", "last_name": "Ivanov", "email": "foo@example.com"}
+# )
