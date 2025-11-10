43c0ce94c0cd1c74cd0f8e502e08a8439b7dd945
k0rsakov <korsakov.iyu@gmail.com>
2025-11-10 09:10:34 +0300

    Изменён проект pet_project_testing_for_data_engineer: - Добавлены маркеры для тестов

--- tests/dags/extensions_for_orchestration/test_extensions_dict.py
+++ tests/dags/extensions_for_orchestration/test_extensions_dict.py
@@ -1,3 +1,4 @@
+import pytest
 from extensions_for_orchestration.extensions_dict import (
     dict_keys_in_str,
     dict_keys_in_placeholder,
@@ -7,7 +8,7 @@ from extensions_for_orchestration.extensions_dict import (
 class TestDictKeysInStr:
     def test_empty_dict(self):
         assert dict_keys_in_str({}) == ""
-
+    @pytest.mark.unit
     def test_none(self):
         assert dict_keys_in_str(None) == ""
 
