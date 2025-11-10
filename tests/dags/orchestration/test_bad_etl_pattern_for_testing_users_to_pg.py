a9cc3c43385695446d86fe38c972f535b0b219a0
k0rsakov <korsakov.iyu@gmail.com>
2025-11-10 06:12:33 +0300

    Изменён проект pet_project_testing_for_data_engineer: - Добавлены тесты для dag – bad_etl_pattern_for_testing_users_to_pg

--- tests/dags/orchestration/test_bad_etl_pattern_for_testing_users_to_pg.py
+++ tests/dags/orchestration/test_bad_etl_pattern_for_testing_users_to_pg.py
@@ -2,42 +2,40 @@ import pytest
 from airflow.models import DagBag
 from airflow.operators.python import PythonOperator
 
-@pytest.fixture(scope="module")
-def dagbag():
-    # Укажи актуальный путь, если он отличается!
-    return DagBag(dag_folder="dags/orchestration", include_examples=False)
+class TestBadEtlPatternDAG:
+    DAG_ID = "bad_etl_pattern_for_testing_users_to_pg"
 
-def test_dag_loaded(dagbag):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    assert dag is not None, "DAG не найден"
-    assert dagbag.import_errors == {}
+    @pytest.fixture(scope="class")
+    def dag_fixture(self):
+        bag = DagBag(dag_folder="dags/orchestration", include_examples=False)
+        dag = bag.get_dag(self.DAG_ID)
+        assert dag is not None, f"DAG {self.DAG_ID} не найден"
+        assert bag.import_errors == {}
+        return dag
 
-def test_task_count(dagbag):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    assert len(dag.tasks) == 3
+    def test_dag_loaded(self, dag_fixture):
+        assert dag_fixture.dag_id == self.DAG_ID
 
-def test_task_ids(dagbag):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    assert set(dag.task_ids) == {'start', 'etl_load_user_to_pg', 'end'}
+    def test_task_count(self, dag_fixture):
+        assert len(dag_fixture.tasks) == 3
 
-def test_etl_task_type(dagbag):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    task = dag.get_task('etl_load_user_to_pg')
-    assert isinstance(task, PythonOperator)
+    def test_task_ids(self, dag_fixture):
+        assert set(dag_fixture.task_ids) == {"start", "etl_load_user_to_pg", "end"}
 
-def test_dependencies(dagbag):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    start = dag.get_task('start')
-    etl = dag.get_task('etl_load_user_to_pg')
-    end = dag.get_task('end')
+    def test_etl_task_type(self, dag_fixture):
+        task = dag_fixture.get_task("etl_load_user_to_pg")
+        assert isinstance(task, PythonOperator)
 
-    # Проверяем зависимости
-    assert etl in start.downstream_list
-    assert end in etl.downstream_list
-    assert start in etl.upstream_list or not start.upstream_list  # start может не иметь апстримов
-    assert etl in end.upstream_list
+    def test_dependencies(self, dag_fixture):
+        start = dag_fixture.get_task("start")
+        etl = dag_fixture.get_task("etl_load_user_to_pg")
+        end = dag_fixture.get_task("end")
+        # Проверяем направленную цепочку
+        assert etl in start.downstream_list
+        assert end in etl.downstream_list
+        assert start in etl.upstream_list or not start.upstream_list  # start может быть первым
+        assert etl in end.upstream_list
 
-@pytest.mark.parametrize("task_id", ["start", "etl_load_user_to_pg", "end"])
-def test_task_exists(dagbag, task_id):
-    dag = dagbag.get_dag('bad_etl_pattern_for_testing_users_to_pg')
-    assert dag.has_task(task_id)
+    @pytest.mark.parametrize("task_id", ["start", "etl_load_user_to_pg", "end"])
+    def test_task_exists(self, dag_fixture, task_id):
+        assert dag_fixture.has_task(task_id)
